83151f63c78456886979f7f3dd72c617e98e4ac7
Romain SERVIERES <romain@madeformed.com>
2024-11-29 16:36:53 +0100

    redir www

---
+++ redir_www.go
@@ -0,0 +1,72 @@
+package redirwww
+
+import (
+	"net"
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"go.uber.org/zap"
+)
+
+func init() {
+	caddy.RegisterModule(RedirWww{})
+	httpcaddyfile.RegisterHandlerDirective("redir_www", parseCaddyfile)
+}
+
+// RedirWww is a redirirection for non-www websites.
+type RedirWww struct{
+	logger *zap.Logger
+}
+
+// CaddyModule returns the Caddy module information.
+func (RedirWww) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.redir_www",
+		New: func() caddy.Module { return new(RedirWww) },
+	}
+}
+
+func (rd *RedirWww) Provision(ctx caddy.Context) error {
+	rd.logger = ctx.Logger() // g.logger is a *zap.Logger
+
+	return nil
+}
+
+func (rd RedirWww) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	response, err := net.LookupTXT("_redirwww." + r.Host)
+
+	if err != nil || len(response) == 0 {
+		rd.logger.Info("error", zap.String("host", "_redirwww." + r.Host), zap.Error(err))
+		return next.ServeHTTP(w, r)
+	}
+
+	rd.logger.Info("redir_www", zap.String("host", r.Host), zap.String("response", response[0]))
+
+	w.Header().Set("Location", response[0])
+	w.WriteHeader(301)
+
+	return next.ServeHTTP(w, r)
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
+func (rd *RedirWww) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	return nil
+}
+
+// parseCaddyfile unmarshals tokens from h into a new RedirWww.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var rd RedirWww
+	err := rd.UnmarshalCaddyfile(h.Dispenser)
+	return rd, err
+}
+
+// Interface guard
+var (
+	_ caddy.Provisioner           = (*RedirWww)(nil)
+	// _ caddy.Validator             = (*RedirWww)(nil)
+	_ caddyhttp.MiddlewareHandler = (*RedirWww)(nil)
+	_ caddyfile.Unmarshaler       = (*RedirWww)(nil)
+)
